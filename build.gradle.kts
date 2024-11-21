dad97e3afbcabfee3ee0412a031959907e9e231b
appsheavenpg <75860066+appsheavenpg@users.noreply.github.com>
2024-11-22 00:23:24 +0100

    feat: sonar qube (#4)
    * feat: sonar qube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
 plugins {
-    java
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.sonarqube)
+}
+
+repositories {
+    mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "appsheavenpg_keep-up-server-aws")
+        property("sonar.organization", "apps-heaven")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
 }
