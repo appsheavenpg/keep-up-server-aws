f7fa4ee5e5424cb9c915e06ee9c8507d4645ee2c
appsheavenpg <75860066+appsheavenpg@users.noreply.github.com>
2024-11-22 23:57:12 +0100

    refactor: replaced sonar keys with .env properties (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.sonarqube)
@@ -7,11 +9,19 @@ repositories {
     mavenCentral()
 }
 
+var env = Properties().apply {
+    file(".env").takeIf { it.exists() }?.inputStream()?.use { load(it) }
+}
+
+val projectKey = env.getProperty("SONAR_PROJECT_KEY")
+val organization = env.getProperty("SONAR_ORGANIZATION")
+val hostUrl = env.getProperty("SONAR_HOST_URL")
+
 sonar {
     properties {
-        property("sonar.projectKey", "appsheavenpg_keep-up-server-aws")
-        property("sonar.organization", "apps-heaven")
-        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.projectKey", projectKey)
+        property("sonar.organization", organization)
+        property("sonar.host.url", hostUrl)
     }
 }
 
