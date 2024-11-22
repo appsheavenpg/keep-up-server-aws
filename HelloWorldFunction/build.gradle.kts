b8c38207cb5b0f1e5860619ffa9ab81a1b241f4b
Paweł Godlewski <appsheavenpg@gmail.com>
2024-11-23 00:45:16 +0100

    test: moved detekt to submodule

--- HelloWorldFunction/build.gradle.kts
+++ HelloWorldFunction/build.gradle.kts
@@ -1,5 +1,8 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.detekt)
 }
 
 group = "com.appsheaven.pl"
@@ -12,6 +15,19 @@ repositories {
 dependencies {
     implementation(libs.bundles.aws.lambda.java)
     testImplementation(kotlin("test"))
+}
+
+detekt {
+    toolVersion = "1.23.7"
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        sarif.required.set(true)
+        sarif.outputLocation.set(file("build/reports/detekt/detekt.sarif"))
+    }
 }
 
 tasks.test {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import io.gitlab.arturbosch.detekt.Detekt
 import java.util.Properties
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.sonarqube)
-    alias(libs.plugins.detekt)
 }
 
 repositories {
@@ -24,24 +22,6 @@ sonar {
         property("sonar.projectKey", projectKey)
         property("sonar.organization", organization)
         property("sonar.host.url", hostUrl)
-    }
-}
-
-allprojects {
-    apply(plugin = libs.plugins.detekt.get().pluginId)
-    
-    detekt {
-        toolVersion = "1.23.7"
-        config.setFrom(file("config/detekt/detekt.yml"))
-        buildUponDefaultConfig = true
-    }
-    
-    tasks.withType<Detekt>().configureEach {
-        reports {
-            sarif.required.set(true)
-            // Store all reports in root project's build directory
-            sarif.outputLocation.set(rootProject.layout.buildDirectory.file("reports/detekt/${project.name}-detekt.sarif"))
-        }
     }
 }
 
