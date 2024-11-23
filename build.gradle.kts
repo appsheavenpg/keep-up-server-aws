1822d98fdcfe21ec6536ed9b45a3562038be7b10
appsheavenpg <75860066+appsheavenpg@users.noreply.github.com>
2024-11-23 23:16:57 +0100

    feat: added detekt with default config (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import java.util.Properties
+import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.sonarqube)
+    alias(libs.plugins.detekt)
 }
 
 repositories {
@@ -22,6 +24,22 @@ sonar {
         property("sonar.projectKey", projectKey)
         property("sonar.organization", organization)
         property("sonar.host.url", hostUrl)
+    }
+}
+
+detekt {
+    config.setFrom(file("./.detekt/detekt.yml"))
+    source.setFrom(
+        file("HelloWorldFunction/src/main/java"),
+        file("HelloWorldFunction/src/test/java"),
+    )
+    buildUponDefaultConfig = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        sarif.required.set(true)
+        sarif.outputLocation.set(file("build/reports/detekt/detekt.sarif"))
     }
 }
 
