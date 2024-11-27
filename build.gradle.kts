f4f9730303ffd7cd04e79a40546687b14a6bdfee
appsheavenpg <75860066+appsheavenpg@users.noreply.github.com>
2024-11-27 23:07:44 +0100

    test: created an exception for sonar to catch on cloud (#7)
    * feat: added sonar sources

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import java.util.Properties
-import io.gitlab.arturbosch.detekt.Detekt
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.sonarqube)
@@ -11,36 +8,28 @@ repositories {
     mavenCentral()
 }
 
-var env = Properties().apply {
-    file(".env").takeIf { it.exists() }?.inputStream()?.use { load(it) }
-}
+val projectKey: String = "appsheavenpg_keep-up-server-aws"
+val organization: String = "apps-heaven"
+val hostUrl: String = "https://sonarcloud.io"
 
-val projectKey = env.getProperty("SONAR_PROJECT_KEY")
-val organization = env.getProperty("SONAR_ORGANIZATION")
-val hostUrl = env.getProperty("SONAR_HOST_URL")
+val sources = listOf(
+    "HelloWorldFunction/src/main/java",
+    "HelloWorldFunction/src/test/java"
+)
 
 sonar {
     properties {
         property("sonar.projectKey", projectKey)
         property("sonar.organization", organization)
         property("sonar.host.url", hostUrl)
+        property("sonar.sources", sources)
     }
 }
 
 detekt {
     config.setFrom(file("./.detekt/detekt.yml"))
-    source.setFrom(
-        file("HelloWorldFunction/src/main/java"),
-        file("HelloWorldFunction/src/test/java"),
-    )
+    source.setFrom(source.map { file(it) })
     buildUponDefaultConfig = true
-}
-
-tasks.withType<Detekt>().configureEach {
-    reports {
-        sarif.required.set(true)
-        sarif.outputLocation.set(file("build/reports/detekt/detekt.sarif"))
-    }
 }
 
 kotlin {
